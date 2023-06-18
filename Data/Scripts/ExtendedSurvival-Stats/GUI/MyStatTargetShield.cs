0ac306679da66f95a0c22e236ffe67eb46dcc11f
ogrimdooh <ogrimdooh@gmail.com>
2023-06-18 11:03:44 -0300

    Adição de shield do target no hud

---
+++ Data/Scripts/ExtendedSurvival-Stats/GUI/MyStatTargetShield.cs
@@ -0,0 +1,17 @@
+﻿namespace ExtendedSurvival.Stats
+{
+    public class MyStatTargetShield : MyTargetStatSimpleProgressBase
+    {
+
+        protected override string GetStatName()
+        {
+            return "EnergyShield";
+        }
+
+        protected override string GetId()
+        {
+            return "target_shield";
+        }
+
+    }
+}
---
+++ Data/Scripts/ExtendedSurvival-Stats/GUI/MyStatTargetShieldDesc.cs
@@ -0,0 +1,19 @@
+﻿namespace ExtendedSurvival.Stats
+{
+    public class MyStatTargetShieldDesc : MyTargetStatSimpleProgressBase
+    {
+
+        protected override string GetStatName()
+        {
+            return "EnergyShield";
+        }
+
+        protected override string GetId()
+        {
+            return "target_shield_desc";
+        }
+
+        public override string ToString() => StatsConstants.GetValidStatsDescription(StatsConstants.ValidStats.EnergyShield);
+
+    }
+}
